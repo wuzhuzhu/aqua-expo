caa13b5b096a99ab45ae16b87e3c93e717719711
太鼓 <wt183603@alibaba-inc.com>
2022-05-16 04:02:46 +0800

    [feature]animated lib moti & expo-web config

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -16,6 +16,7 @@ import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
+import MotiScreen from '../screens/moti-demo';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
@@ -90,6 +91,14 @@ function BottomTabNavigator() {
         options={{
           title: 'Tab Two',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="MotiDemo"
+        component={TabTwoScreen}
+        options={{
+          title: '动画demo',
+          tabBarIcon: ({ color }) => <TabBarIcon name="map" color={color} />,
         }}
       />
     </BottomTab.Navigator>
