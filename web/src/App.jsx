737eca8c291e8831435a7743699a2b620ae99b40
太鼓 <wt183603@alibaba-inc.com>
2022-05-24 17:28:17 +0800

    [demo]minor fix for web project, a failure on trying use flexbox order on react-native(RN not supported)

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PdfPage from "./pages/pdf";
+import Demo from "./pages/demo";
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Root />} />
         <Route path="pdf/:encodedUri/:page" element={<PdfPage />} />
+        <Route path="demo" element={<Demo />} />
       </Routes>
     </BrowserRouter>
   )
