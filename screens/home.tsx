e7809ed38c5601591ed2fb7adab4ab4f260b8cbb
太鼓 <wt183603@alibaba-inc.com>
2022-07-08 15:59:10 +0800

    [feat]add carousel and mock data

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,35 +1,109 @@
-import {Box, Column, Flex, Center, Text, Image, Input, AspectRatio, Icon} from 'native-base'
-import {MaterialIcons} from "@expo/vector-icons"
+import {
+	Box,
+	Column,
+	Flex,
+	Center,
+	Text,
+	Image,
+	Input,
+	AspectRatio,
+	Icon,
+} from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+import Carousel from "react-native-snap-carousel";
 
-import {HOMEPAGE_BTNS} from '../constants/Basic'
-import {RootStackScreenProps} from "../types"
-import logoImg from '../assets/images/logo.png'
-import HomeScreenCard from '../components/home/card-btn'
-import Logo from '../components/home/logo'
+import { HOMEPAGE_BTNS } from "../constants/Basic";
+import { RootStackScreenProps } from "../types";
+import logoImg from "../assets/images/logo.png";
+import HomeScreenCard from "../components/home/card-btn";
+import Logo from "../components/home/logo";
+import { windowWidth } from "../utils/helper";
+import React, { useRef, useState } from "react";
 
-export default function HomeScreen({navigation}: RootStackScreenProps<'Home'>) {
-  return (
-    <Box safeArea>
-      <Column justifyContent="center" space={4}>
-        <Logo imageMaxWidth="70%" mt={4} />
-        <Center>
-          <Text onPress={() => navigation.navigate('Modal2')} textAlign="center" maxWidth="60%" color="trueGray.600">Premium Rendered Proteins for Sustainable Aquaculture in the World</Text>
-        </Center>
-        <Center>
-          <Input size="10" w="80%" mt="2" InputLeftElement={<Icon as={<MaterialIcons name="search" />} size={5} ml="2" color="muted.400" />} placeholder="Search For ..." />
-        </Center>
-        <Center mt={2} mx={2} >
-          <Flex direction="row">
-            <HomeScreenCard {...HOMEPAGE_BTNS[0]} navigation={navigation} />
-            <HomeScreenCard {...HOMEPAGE_BTNS[1]} navigation={navigation} />
-          </Flex>
-          <Flex direction="row">
-            <HomeScreenCard {...HOMEPAGE_BTNS[2]} navigation={navigation} />
-            <HomeScreenCard {...HOMEPAGE_BTNS[3]} navigation={navigation} />
-          </Flex>
-        </Center>
-      </Column>
+type ICarouselType = {
+	id: number;
+	title: string;
+	img: string;
+	description?: string;
+};
 
-    </Box>
-  )
+function CarouselItem({ item, index }: { item: ICarouselType; index: number }) {
+	return (
+		<AspectRatio w="300" ratio={2 / 1}>
+			<Image
+				source={{
+					uri: item?.img,
+				}}
+				borderRadius={8}
+				alt={item?.title || "img"}
+			/>
+		</AspectRatio>
+	);
+}
+
+export default function HomeScreen({
+	navigation,
+}: RootStackScreenProps<"Home">) {
+	const carouselRef = useRef(null);
+	const [currentIndex, setCurrentIndex] = useState(null as number | null);
+	const mockBanners = [
+		{
+			id: 1,
+			title: "The expo app launched finally",
+			description: "The expo app launched finally, congres everybody!",
+			img: "https://picsum.photos/id/1/640/240",
+		},
+		{
+			id: 2,
+			title: "The expo app launched finally2",
+			description:
+				"The expo app launched finally, congres everybody!, \r\n coaslkdjfljads;lfjsdlf ;asdfjkladsjflkasjdflkjasdlkfjadsjkljl;fdsajlkfjasl;j;;dsaf;ds;afadsf;fddsafdsafdafs",
+			img: "https://source.unsplash.com/random/640x240",
+		},
+		{
+			id: 3,
+			title: "The expo app launched finally2",
+			description:
+				"The expo app launched finally, congres everybody!, \r\n coaslkdjfljads;lfjsdlf ;asdfjkladsjflkasjdflkjasdlkfjadsjkljl;fdsajlkfjasl;j;;dsaf;ds;afadsf;fddsafdsafdafs",
+			img: "https://source.unsplash.com/random/640x300",
+		},
+	];
+	return (
+		<Box safeArea>
+			<Column justifyContent="center" space={4}>
+				<Logo imageMaxWidth="70%" mt={4} />
+				<Center>
+					<Text
+						onPress={() => navigation.navigate("Modal2")}
+						textAlign="center"
+						maxWidth="60%"
+						color="trueGray.600"
+					>
+						Premium Rendered Proteins for Sustainable Aquaculture in the World
+					</Text>
+				</Center>
+				<Center>
+					<Carousel
+						layout={"default"}
+						ref={carouselRef}
+						data={mockBanners}
+						sliderWidth={375}
+						itemWidth={300}
+						renderItem={CarouselItem}
+						onSnapToItem={(index: number) => setCurrentIndex(index)}
+					/>
+				</Center>
+				<Center mt={2} mx={2}>
+					<Flex direction="row">
+						<HomeScreenCard {...HOMEPAGE_BTNS[0]} navigation={navigation} />
+						<HomeScreenCard {...HOMEPAGE_BTNS[1]} navigation={navigation} />
+					</Flex>
+					<Flex direction="row">
+						<HomeScreenCard {...HOMEPAGE_BTNS[2]} navigation={navigation} />
+						<HomeScreenCard {...HOMEPAGE_BTNS[3]} navigation={navigation} />
+					</Flex>
+				</Center>
+			</Column>
+		</Box>
+	);
 }
