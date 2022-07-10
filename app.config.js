d6ddbf3d8250b3a317a8dd31168c68efe8462e06
太鼓 <wt183603@alibaba-inc.com>
2022-07-11 01:40:53 +0800

    [feat]update version

--- app.config.js
+++ app.config.js
@@ -1,38 +1,36 @@
 const IS_DEV = process.env.APP_VARIANT === "development";
 
 export default {
-  "expo": {
-    "name": IS_DEV ? "aqua-expo(dev)" : "aqua-expo",
-    "slug": "aqua-expo",
-    "version": "1.0.1",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "myapp",
-    "userInterfaceStyle": "automatic",
-    "splash": {
-      "image": "./assets/images/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
-    },
-    "updates": {
-      "fallbackToCacheTimeout": 0
-    },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": IS_DEV ? "com.nara.aquaexpo-dev" : "com.nara.aquaexpo",
-    },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
-      },
-      "package": IS_DEV ? "com.nara.aquaexpo" : "com.nara.aquaexpo",
-    },
-    "web": {
-      "favicon": "./assets/images/favicon.png"
-    }
-  }
-}
+	expo: {
+		name: IS_DEV ? "aqua-expo(dev)" : "aqua-expo",
+		slug: "aqua-expo",
+		version: "1.0.2",
+		orientation: "portrait",
+		icon: "./assets/images/icon.png",
+		scheme: "myapp",
+		userInterfaceStyle: "automatic",
+		splash: {
+			image: "./assets/images/splash.png",
+			resizeMode: "contain",
+			backgroundColor: "#ffffff",
+		},
+		updates: {
+			fallbackToCacheTimeout: 0,
+		},
+		assetBundlePatterns: ["**/*"],
+		ios: {
+			supportsTablet: true,
+			bundleIdentifier: IS_DEV ? "com.nara.aquaexpo-dev" : "com.nara.aquaexpo",
+		},
+		android: {
+			adaptiveIcon: {
+				foregroundImage: "./assets/images/adaptive-icon.png",
+				backgroundColor: "#ffffff",
+			},
+			package: IS_DEV ? "com.nara.aquaexpo" : "com.nara.aquaexpo",
+		},
+		web: {
+			favicon: "./assets/images/favicon.png",
+		},
+	},
+};
